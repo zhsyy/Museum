3fcd8034df39560c1603c44e31dfde67ce61aa00
张思源 <17302010053@fudan.edu.cn>
2019-07-15 20:33:20 +0800

    添加了连接数据库的包

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -5,13 +5,13 @@ import dto.LoginUser;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "login", value = "/login")
+//@WebServlet(name = "login", value = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
