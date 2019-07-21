14f8fdac804dc3bc6638b0b8c792724b94429e93
HNoodles <13671980486@163.com>
2019-07-21 14:47:34 +0800

    modified user service imp

--- src/service/impl/UserServiceImp.java
+++ src/service/impl/UserServiceImp.java
@@ -1,16 +1,21 @@
 package service.impl;
 
+import dao.FriendshipDao;
 import dao.UsersDao;
+import dao.impl.FriendshipDaoImp;
 import dao.impl.UsersDaoImp;
+import entity.FriendshipEntity;
 import entity.UsersEntity;
 import service.UserService;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class UserServiceImp implements UserService {
     private UsersDao usersDao = new UsersDaoImp();
+    private FriendshipDao friendshipDao = new FriendshipDaoImp();
 
     @Override
     public UsersEntity login(String username, String password) {
@@ -56,6 +61,7 @@ public class UserServiceImp implements UserService {
     @Override
     public boolean checkModifyUser(int userId, String password) {
         return usersDao.query(userId, password) != null;
+    }
 
     @Override
     public void addUser(UsersEntity user) {
@@ -76,5 +82,32 @@ public class UserServiceImp implements UserService {
     @Override
     public void deleteUser(int userId) {
         usersDao.delete(userId);
+    }
+
+    @Override
+    public List<UsersEntity> getFriends(int userId) {
+        List<UsersEntity> friends = new ArrayList<>();
+
+        // get all friendships of user
+        List<FriendshipEntity> friendships = friendshipDao.getFriendships(userId);
+        for (FriendshipEntity friendship : friendships) {
+            int friendUserId = friendship.getSenderId() == userId ? friendship.getReceiverId() : friendship.getSenderId();
+            friends.add(usersDao.query(friendUserId));
+        }
+
+        return friends;
+    }
+
+    @Override
+    public List<UsersEntity> getFriendRequestSenders(int receiverId) {
+        List<UsersEntity> senders = new ArrayList<>();
+
+        // get all friendship requests
+        List<FriendshipEntity> requests = friendshipDao.getFriendshipRequests(receiverId);
+        for (FriendshipEntity request : requests) {
+            senders.add(usersDao.query(request.getSenderId()));
+        }
+
+        return senders;
     }
 }
